b808292d3ab9533e10257a0e2503fed0e35f7c03
PenguWoodGanon <maxwellj2022@icloud.com>
2026-05-18 14:12:04 -0600

    Updated The Ino Code

--- SO26Compass/SO26Compass.ino
+++ SO26Compass/SO26Compass.ino
@@ -1,114 +1,27 @@
-// Created by Max Heywood and the Jack and Co Team
 #include <Servo.h>
 
-Servo yawServo;
+Servo myServo;
 
-//////////////////////////////
-//        Variables
-//////////////////////////////
-char message;
-int position = 90;
-
-//////////////////////////////
-//          Setup
-//////////////////////////////
 void setup() {
-  yawServo.attach(2);
   Serial.begin(9600);
-  
-  clearSerial();
-  homeServos(); // Now defined below
-  
-  Serial.println("Loading...");
-  delay(1000);
-  Serial.println("Welcome to Science Olympics IDE (Compass)");
-  Serial.println("Type h for help");
+  myServo.attach(2);  // Servo signal wire on pin 9
+  Serial.println("Welcome to Science Olympics IDE - Compass");
+  Serial.println("Enter angle (0-180):");
 }
 
-//////////////////////////////
-//          Loop
-//////////////////////////////
 void loop() {
   if (Serial.available() > 0) {
-    message = Serial.read();
-
-    // Start
-    if (message == 's') {
-      Serial.println("Starting...");
-    }
-    // Help menu
-    else if (message == 'h') {
-      helpMenu();
-    }
-    // Set Move - Now calls the functional setAngle()
-    else if (message == 'm') {
-      setAngle();
-    }
-    else if (message == 'q') {
-      quit();
-    }
-    else if (message == 'x') {
-      homeServos();
-    }
-    else if (message == 'e') {
-      danceParty();
+    int angle = Serial.parseInt();
+    
+    // Clear any leftover newline/carriage return
+    while (Serial.available()) Serial.read();
+    
+    if (angle >= 0 && angle <= 180) {
+      myServo.write(angle);
+      Serial.print("Moved to: ");
+      Serial.println(angle);
+    } else {
+      Serial.println("Invalid. Enter 0-180.");
     }
   }
-}
-
-//////////////////////////////
-//        Functions
-//////////////////////////////
-
-void homeServos() {
-  Serial.println("Homing Servo to 90...");
-  position = 90;
-  yawServo.write(position);
-}
-
-void setAngle() {
-  Serial.println("Enter Angle (0-180) and press Enter:");
-
-  while (Serial.available() == 0) {}
-
-  int newPos = Serial.parseInt();
-  delay(10);
-  while (Serial.available() > 0) Serial.read();
-
-  if (newPos >= 0 && newPos <= 180) {
-    position = newPos;
-    yawServo.write(position);
-    Serial.print("Angle: ");
-    Serial.println(position);
-  } else {
-    Serial.println("Invalid Angle! Please use 0-180.");
-  }
-}
-void clearSerial() {
-  for (int i = 0; i < 15; i++) {
-    Serial.println();
-  }
-}
-
-void quit() {
-  Serial.println("Quitting. Restart Arduino to reconnect.");
-  Serial.end();
-}
-
-void helpMenu() {
-  Serial.println("\n--- Controls ---");
-  Serial.println("s - Start");
-  Serial.println("m - Set Angle (Followed by a number)");
-  Serial.println("x - Home servo");
-  Serial.println("e - Dance");
-  Serial.println("q - Quit");
-}
-
-void danceParty() {
-  Serial.println("Dancing!");
-  for (int i = 0; i <= 180; i += 20) {
-    yawServo.write(i);
-    delay(100);
-  }
-  homeServos();
 }
